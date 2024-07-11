12f06c3a3f424893b77db0232917db62baf0f195
HenriqueGBA <gabgba90@gmail.com>
2024-07-11 12:07:41 -0300

    realização do 3 exercicio

--- TestJava/src/App.java
+++ TestJava/src/App.java
@@ -1,7 +1,9 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc =  new Scanner(System.in);
 //first question 1. Given a list of strings, write a method that returns a list of all strings that start with the letter ‘a’
 // (lower case) and have exactly 3 letters. TIP: Use Java Lambdas and Streams API’s.
         ListaFiltro listaFiltro = new ListaFiltro();
@@ -11,14 +13,32 @@ public class App {
         List<String> listaFiltrada = listaFiltro.filtro(strings);
         System.out.println("Resultado:");
         System.out.println(listaFiltrada);
-//second question Write a Java program to remove all white spaces from a string without using replace().
         RemoverEspacoBranco removerEspacoBranco = new RemoverEspacoBranco();
-        
+//second question Write a Java program to remove all white spaces from a string without using replace().  
         String palavra = "Ola mundo meu nome e henrique";
 
         String palavraSemEspaco = removerEspacoBranco.removerEspacos(palavra);
         System.out.println("Resultado:");
         System.out.println(palavra);
         System.out.println(palavraSemEspaco);
+//Write a Java program to swap two numbers without using the third variable.
+        System.out.println("Digite o primeiro numero:");
+        int num1 = sc.nextInt();
+
+        
+        System.out.println("Digite o segundo numero:");
+        int num2 = sc.nextInt();
+
+        System.out.println("sem trocar");
+        System.out.printf("Este e o numero 1: "+ num1 + "\n"); 
+        System.out.printf("Este e o numero 2: "+ num2); 
+        
+        num1 += num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println("\nTrocando:");
+        System.out.printf("Este e o numero 1: " + num1 + "\n"); 
+        System.out.printf("Este e o numero 2: " + num2); 
     }
 }
